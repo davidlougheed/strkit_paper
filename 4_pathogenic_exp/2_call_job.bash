878824f5ad9f602546bc85f2fc1ec19a30941996
David Lougheed <david.lougheed@gmail.com>
2024-01-30 15:05:26 -0500

    fix: calling strkit + time genotyping

--- 4_pathogenic_exp/2_call_job.bash
+++ 4_pathogenic_exp/2_call_job.bash
@@ -5,12 +5,12 @@
 #SBATCH --time=0-8
 #SBATCH --account=rrg-bourqueg-ad
 
-module load python/3.9
+module load python/3.9 parasail
 
-#python3 ./call_longtr.py
+#time python3 ./call_longtr.py
 
 source ../envs/env_strkit/bin/activate
-python3 ./call_strkit.py
+time python3 ./call_strkit.py
 deactivate
 
-python3 ./call_trgt.py
+time python3 ./call_trgt.py
