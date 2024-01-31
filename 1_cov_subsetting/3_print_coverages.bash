fc67a93e2a83b91659ea6f038064acf9020808de
David Lougheed <david.lougheed@gmail.com>
2024-01-31 11:18:51 -0500

    index bams before depth calc in step 1

--- 1_cov_subsetting/3_print_coverages.bash
+++ 1_cov_subsetting/3_print_coverages.bash
@@ -2,17 +2,19 @@
 #SBATCH --mem=16G
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=0-08
+#SBATCH --time=0-18
 #SBATCH --account=rrg-bourqueg-ad
 
 module load samtools
 
 for bf in ./data/hifi/*; do
   echo "Working on $bf"
-  samtools depth -a "$bf"  |  awk '{sum+=$3} END { print "Average = ",sum/NR}'
+  samtools index "$bf"
+  samtools depth -a "$bf" | awk '{sum+=$3} END { print "Average = ",sum/NR}'
 done
 
 for bf in ./data/ont/*; do
   echo "Working on $bf"
-  samtools depth -a "$bf"  |  awk '{sum+=$3} END { print "Average = ",sum/NR}'
+  samtools index "$bf"
+  samtools depth -a "$bf" | awk '{sum+=$3} END { print "Average = ",sum/NR}'
 done
