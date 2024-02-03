a34cfec44a2d58bb5bbb151a514901b628f04fa3
David Lougheed <david.lougheed@gmail.com>
2024-02-03 16:29:58 -0500

    dont do needless alignments in alignment script

--- 1_cov_subsetting/3_align.bash
+++ 1_cov_subsetting/3_align.bash
@@ -3,11 +3,15 @@
 REF="./data/ref/hg38.analysisSet.fa"
 
 for bf in ./data/hifi/HG00?.bam; do
-  echo "Submitting alignment job for ${bf}"
-  sbatch --export="BAM=${bf},REF=${REF},TECH=hifi" ./align_job.bash
+  if [[ ! -f "${bf%.*}.aligned.bam" ]]; then
+    echo "Submitting alignment job for ${bf}"
+    sbatch --export="BAM=${bf},REF=${REF},TECH=hifi" ./align_job.bash
+  fi
 done
 
 for bf in ./data/ont/HG00?.bam; do
-  echo "Submitting alignment job for ${bf}"
-  sbatch --export="BAM=${bf},REF=${REF},TECH=ont" ./align_job.bash
+  if [[ ! -f "${bf%.*}.aligned.bam" ]]; then
+    echo "Submitting alignment job for ${bf}"
+    sbatch --export="BAM=${bf},REF=${REF},TECH=ont" ./align_job.bash
+  fi
 done
