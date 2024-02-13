1471584664c6cf02c5938377258e9e0d99868df2
David Lougheed <david.lougheed@gmail.com>
2024-02-13 09:42:53 -0500

    fastq generation work

--- 1_cov_subsetting/align_job.bash
+++ 1_cov_subsetting/align_job.bash
@@ -5,28 +5,23 @@
 #SBATCH --time=1-04
 #SBATCH --account=rrg-bourqueg-ad
 
-module load bedtools samtools
+module load samtools
 
 echo "BAM=${BAM}"
 echo "REF=${REF}"
 echo "TECH=${TECH}"
-fastq="${BAM%.*}.fq"
 fastq_gz="${fastq}.gz"
 echo "fastq=${fastq}"
 
-if [[ ! -f "${fastq}" ]]; then
-  bedtools bamtofastq -i "${BAM}" -fq "${fastq}" || exit
-fi
-
 if [[ ! -f "${fastq_gz}" ]]; then
   tmp="${SLURM_TMPDIR}/fastq.gz"
-  gzip -3 -c "${fastq}" > "${tmp}"
+  samtools fastq -@ 10 | pigz -p 10 -3 > "${tmp}"
   mv "${tmp}" "${fastq_gz}"
 fi
 
 tmp_bam="${SLURM_TMPDIR}/out.bam"
 aligned_bam="${BAM%.*}.aligned.bam"
 
-../bin/minimap2 -t 12 -ax "map-${TECH}" "${REF}" "${fastq_gz}" | samtools sort --write-index -@ 12 - -o "${tmp_bam}"
+../bin/minimap2 -t 10 -ax "map-${TECH}" "${REF}" "${fastq_gz}" | samtools sort --write-index -@ 10 - -o "${tmp_bam}"
 mv "${tmp_bam}" "${aligned_bam}"
 chgrp rrg-bourqueg-ad "${aligned_bam}"
