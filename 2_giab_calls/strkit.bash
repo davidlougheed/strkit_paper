15c9551e2688ac132b468c23307502c7f7da7dc9
David Lougheed <david.lougheed@gmail.com>
2024-02-12 23:28:04 -0500

    strkit more opt

--- 2_giab_calls/strkit.bash
+++ 2_giab_calls/strkit.bash
@@ -12,12 +12,16 @@ bam_tmpdir="${SLURM_TMPDIR}/reads.bam"
 cp "${BAM}" "${bam_tmpdir}"
 cp "${BAM}.bai" "${bam_tmpdir}.bai"
 
+snv_vcf_tmpdir="${SLURM_TMPDIR}/snvs.vcf.gz"
+cp "./data/00-common_all.vcf.gz" "${snv_vcf_tmpdir}"
+cp "./data/00-common_all.vcf.gz.tbi" "${snv_vcf_tmpdir}.tbi"
+
 /usr/bin/time -o "./out/calls/${TECH}/${SAMPLE}.strkit.time" strkit call \
   --ref "${REF}" \
   --loci ./out/adotto_catalog_strkit.bed \
   --sex-chr "${KARYOTYPE}" \
   --hq \
-  --incorporate-snvs "./data/00-common_all.vcf.gz" \
+  --incorporate-snvs "${snv_vcf_tmpdir}" \
   --vcf "./out/calls/${TECH}/${SAMPLE}.strkit.vcf" \
   --no-tsv \
   --seed "${SEED}" \
