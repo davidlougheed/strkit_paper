0fb3c41e24bdc66f34874921d3b38ae7c0597b11
David Lougheed <david.lougheed@gmail.com>
2024-02-26 11:00:59 -0500

    move normalize script to step 2

---
+++ 2_giab_calls/5_normalize.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --mem=4G
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=0-12
+#SBATCH --account=rrg-bourqueg-ad
+
+REF='../1_cov_subsetting/data/ref/hg38.analysisSet.fa'
+
+truvari_norm () {
+  # Normalizes a .vcf.gz with base path $1 to have bi-allelic entries only (so multi-allelic VCF lines get split up),
+  # which is a requirement for input into Truvari.
+  bcftools norm -f "${REF}" -c s -m - "${1}.vcf.gz" -O z -o "${1}.norm.vcf.gz"
+}
+
+module load bcftools
+
+for f in ./out/calls/**/*.strkit.vcf.gz; do
+  base_path="${f%.*}"
+  base_path="${base_path%.*}"
+  echo "Working on ${f}; base_path=${base_path}"
+  truvari_norm "${base_path}"
+  echo "Done ${f}"
+done
+
+for f in ./out/calls/**/*.trgt.vcf.gz; do
+  base_path="${f%.*}"
+  base_path="${base_path%.*}"
+  echo "Working on ${f}; base_path=${base_path}"
+  truvari_norm "${base_path}"
+  echo "Done ${f}"
+done
--- 3_benchmarking/2_normalize.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# TODO:
-bcftools norm -f /lustre03/project/rrg-bourqueg-ad/dlough2/workdir2022/ref/hg38.analysisSet.fa -c s -m - test.chr4.f100.vcf.gz -o test.chr4.f100.ba.vcf
