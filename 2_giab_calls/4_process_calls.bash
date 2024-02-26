0104d66cdf97d9c0ffa631197bcea7e4fa14ce33
David Lougheed <david.lougheed@gmail.com>
2024-02-26 10:02:33 -0500

    add call processing script

---
+++ 2_giab_calls/4_process_calls.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --mem=8G
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=1-00
+#SBATCH --account=rrg-bourqueg-ad
+
+module load samtools bcftools
+cd out/calls || exit
+
+for f in **/*.strkit.vcf; do
+  bgzip -f $f
+  tabix "${f}.gz"
+done
+
+for f in **/*.trgt.vcf.gz; do
+  mv "${f}" "${f}_old"
+  bcftools sort "${f}_old" -O z -o "${f}" || exit
+  rm "${f}_old"
+  tabix "${f}"
+done
+
+cd ../.. || exit
