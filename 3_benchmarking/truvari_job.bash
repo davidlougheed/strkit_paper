7fdfae5e8c568dbdf1bbd175398919140de475bb
David Lougheed <david.lougheed@gmail.com>
2024-02-26 15:54:51 -0500

    lower truvari job reqs

--- 3_benchmarking/truvari_job.bash
+++ 3_benchmarking/truvari_job.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --mem=16G
+#SBATCH --mem=8G
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=3-00
+#SBATCH --time=1-00
 #SBATCH --account=rrg-bourqueg-ad
 
 module load python/3.9
