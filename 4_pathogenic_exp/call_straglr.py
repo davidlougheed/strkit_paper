8bf83bbc11be2624ef41fc1848bf77e8d88b6907
David Lougheed <david.lougheed@gmail.com>
2024-06-28 12:55:34 -0400

    add pathogenic exp calling for strdust + straglr

---
+++ 4_pathogenic_exp/call_straglr.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+from common import SAMPLE_KTS, BASE_PATH, REF_GENOME, bam
+
+
+def main():
+    for sample, karyotype in SAMPLE_KTS.items():
+        print("Straglr", sample, karyotype)
+        subprocess.check_call([
+            "straglr.py", "call",
+            bam(sample),
+            REF_GENOME,
+            "--loci", str(BASE_PATH / "data" / "catalog.strkit.bed"),
+        ])
+
+
+if __name__ == "__main__":
+    main()
