ee934f4095502c93d5d2d4d289e3a17fdaa26733
David Lougheed <david.lougheed@gmail.com>
2024-10-01 21:00:30 -0400

    make out dir

--- 5_ga4k/1_setup.bash
+++ 5_ga4k/1_setup.bash
@@ -4,10 +4,15 @@ REF_URL="https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.
 REF_FILE="GRCh38_no_alt_analysis_set.fna"
 REF_FILE_GZ="GRCh38_no_alt_analysis_set.fna.gz"
 
+# Download reference genome
 mkdir -p data/ref
-
+cd ./data/ref || exit
 if [[ ! -f "${REF_FILE}" ]]; then
   wget "${REF_URL}" -O "${REF_FILE_GZ}"
   gunzip -c "${REF_FILE_GZ}" > "${REF_FILE}"
   rm "${REF_FILE_GZ}"
 fi
+cd ../.. || exit
+
+# Set up out directory
+mkdir -p out/calls
