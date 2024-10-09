08d667f36d6b5398f6db411957322e9eb219a59a
David Lougheed <david.lougheed@gmail.com>
2024-10-09 08:08:38 -0400

    fix: use strkit env for strkit mi jobs

--- 5_ga4k/strkit_mi_job.bash
+++ 5_ga4k/strkit_mi_job.bash
@@ -7,6 +7,12 @@
 
 mkdir -p ./out/mi
 
+module load StdEnv/2023
+module load python/3.11 scipy-stack/2023b parasail/2.6.2
+source ../envs/env_strkit/bin/activate
+
+export PYTHONOPTIMIZE=1
+
 strkit mi --caller strkit-vcf \
     "./out/calls/${CHILD}.strkit.vcf.gz" \
     "./out/calls/${FATHER}.strkit.vcf.gz" \
@@ -15,3 +21,5 @@ strkit mi --caller strkit-vcf \
     --json "${OUT_JSON}" \
     --test wmw \
     --mt-corr fdr_tsbky
+
+deactivate
