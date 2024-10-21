6027b8f74b40c486fbc7d00b0cd823f31bd3aff7
David Lougheed <david.lougheed@gmail.com>
2024-10-21 16:12:18 -0400

    4: add straglr + strdust to call job

--- 4_pathogenic_exp/2_call_job.bash
+++ 4_pathogenic_exp/2_call_job.bash
@@ -8,10 +8,22 @@
 module load StdEnv/2023
 module load python/3.11 scipy-stack/2023b parasail/2.6.2
 
+# LongTR
 time python3 ./call_longtr.py
 
+# Straglr
+export PATH="${PATH}:${PWD}/../bin"  # for TRF
+source ../envs/env_straglr/bin/activate
+time python3 ./call_straglr.py
+deactivate
+
+# STRdust
+time python3 ./call_strdust.py
+
+# STRkit
 source ../envs/env_strkit/bin/activate
 time python3 ./call_strkit.py
 deactivate
 
+# TRGT
 time python3 ./call_trgt.py
