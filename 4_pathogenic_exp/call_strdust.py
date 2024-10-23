aaa862fea4e78471216812ae16da43a79ea6eb68
David Lougheed <david.lougheed@gmail.com>
2024-10-23 12:38:01 -0400

    4: add strdust pathogenic exp calling script

---
+++ 4_pathogenic_exp/call_strdust.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+from common import SAMPLE_KTS, BASE_PATH, REF_GENOME, bam
+
+
+def main():
+    for sample, karyotype in SAMPLE_KTS.items():
+        print("STRdust", sample, karyotype)
+        with open(BASE_PATH / "out" / f"{sample}.strdust.vcf", "w") as fh:
+            subprocess.check_call([
+                "../bin/strdust",
+                "-R", str(BASE_PATH / "data" / "catalog.strkit.bed"),
+                "--unphased",
+                *(("--haploid", "X,Y") if karyotype == "XY" else ()),
+                REF_GENOME,
+                bam(sample),
+            ], stdout=fh)
+
+
+if __name__ == "__main__":
+    main()
