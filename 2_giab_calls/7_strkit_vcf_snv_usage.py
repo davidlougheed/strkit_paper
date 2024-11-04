0fc7614025180318c2f09d0091c8e5f0ec895631
David Lougheed <david.lougheed@gmail.com>
2024-11-04 12:11:13 -0500

    2: add strkit snv usage script

---
+++ 2_giab_calls/7_strkit_vcf_snv_usage.py
@@ -0,0 +1,31 @@
+from collections import Counter
+from pathlib import Path
+from pysam import VariantFile
+
+
+OUT_DIR = Path(__file__).parent / "out" / "calls"
+TECHS = ("hifi", "ont")
+
+
+def main():
+    for tech in TECHS:
+        for tool in ("strkit", "strkit-no-vcf"):
+            for vp in (OUT_DIR / tech).glob(f"*.{tool}.vcf.gz"):
+                # type_c = Counter()
+                ps_c = Counter()
+                vf = VariantFile(str(vp))
+                for variant in vf:
+                    if variant.info["VT"] != "str":
+                        continue
+                    s = variant.samples[0]
+
+                    ps_c.update((s["PM"],) if "PM" in s else ("not_called",))
+
+                n_strs = ps_c.total()
+
+                for e in ps_c.items():
+                    print(f"{e[0]}: {e[1] / n_strs * 100:.2f}%")
+
+
+if __name__ == "__main__":
+    main()
