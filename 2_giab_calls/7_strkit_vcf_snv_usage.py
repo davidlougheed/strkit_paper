d4796e2ae41403b7ccd67856edfa3992a704b2da
David Lougheed <david.lougheed@gmail.com>
2024-11-04 12:13:33 -0500

    dont try counting snvs in non-snv vcf...

--- 2_giab_calls/7_strkit_vcf_snv_usage.py
+++ 2_giab_calls/7_strkit_vcf_snv_usage.py
@@ -9,23 +9,22 @@ TECHS = ("hifi", "ont")
 
 def main():
     for tech in TECHS:
-        for tool in ("strkit", "strkit-no-vcf"):
-            for vp in (OUT_DIR / tech).glob(f"*.{tool}.vcf.gz"):
-                # type_c = Counter()
-                ps_c = Counter()
-                vf = VariantFile(str(vp))
-                for variant in vf:
-                    if variant.info["VT"] != "str":
-                        continue
-                    s = variant.samples[0]
+        for vp in (OUT_DIR / tech).glob(f"*.strkit.vcf.gz"):
+            # type_c = Counter()
+            ps_c = Counter()
+            vf = VariantFile(str(vp))
+            for variant in vf:
+                if variant.info["VT"] != "str":
+                    continue
+                s = variant.samples[0]
 
-                    ps_c.update((s["PM"],) if "PM" in s else ("not_called",))
+                ps_c.update((s["PM"],) if "PM" in s else ("not_called",))
 
-                n_strs = ps_c.total()
+            n_strs = ps_c.total()
 
-                print(vp)
-                for e in ps_c.items():
-                    print(f"    {e[0]}: {e[1] / n_strs * 100:.2f}%")
+            print(vp)
+            for e in ps_c.items():
+                print(f"    {e[0]}: {e[1] / n_strs * 100:.2f}%")
 
 
 if __name__ == "__main__":
