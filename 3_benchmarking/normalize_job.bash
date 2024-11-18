c8162da8cd705d070b611096f965ff7f3510288e
David Lougheed <david.lougheed@gmail.com>
2024-11-18 12:15:58 -0500

    3: rm unused normalize job file

--- 3_benchmarking/normalize_job.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --mem=4G
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --time=0-08
-#SBATCH --account=rrg-bourqueg-ad
-
-module load bcftools
-bcftools norm -f "${REF}" -c s -m - "${VCF}" -o "${VCF%.*}.norm.vcf"
