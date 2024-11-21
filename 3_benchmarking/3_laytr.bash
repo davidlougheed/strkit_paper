38a994795ad1d9258b3b7cdf0de1a522d59a200f
David Lougheed <david.lougheed@gmail.com>
2024-11-21 12:27:10 -0500

    3: laytr - phased + non-phased

--- 3_benchmarking/3_laytr.bash
+++ 3_benchmarking/3_laytr.bash
@@ -3,17 +3,26 @@
 techs=( hifi ont )
 # Re-enable if not failed
 #tools=( longtr straglr strdust strkit trgt )
-#tools=( longtr strkit strkit-no-snv trgt )
-tools=( longtr strkit trgt )
+tools=( longtr strkit strkit-no-snv trgt )
+
+laytr_job () {
+  tech="${1}"
+  tool="${2}"
+  phased="${3}"
+
+  d="./out/hg002_benchmark/${tech}/${tool}${phased:+_phased}"
+
+  if [[ -d "${d}" ]]; then
+    echo "Running laytr on ${d}"
+    sbatch --export="BENCH_DIR=${d}" ./laytr_job.bash
+  else
+    echo "Skipping job; ${d} does not exist"
+  fi
+}
 
 for tech in "${techs[@]}"; do
   for tool in "${tools[@]}"; do
-    d="./out/hg002_benchmark/${tech}/${tool}"
-    if [[ -d "${d}" ]]; then
-      echo "Running laytr on ${d}"
-      sbatch --export="BENCH_DIR=${d}" ./laytr_job.bash
-    else
-      echo "Skipping job; ${d} does not exist"
-    fi
+    laytr_job "${tech}" "${tool}" ""
+    laytr_job "${tech}" "${tool}" "1"
   done
 done
