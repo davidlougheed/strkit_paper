373d92322bad8c45031fcec4aded04fd0baa5ab9
David Lougheed <david.lougheed@gmail.com>
2024-12-05 10:17:46 -0500

    2: inc strdust time

--- 2_giab_calls/strdust.bash
+++ 2_giab_calls/strdust.bash
@@ -2,7 +2,7 @@
 #SBATCH --mem=180G
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --time=12:00:00
+#SBATCH --time=36:00:00
 #SBATCH --account=rrg-bourqueg-ad
 
 bam_tmpdir="${SLURM_TMPDIR}/reads.bam"
