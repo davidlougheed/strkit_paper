f93f7bce95645ee2762df82e7dc8bbde9c13614e
David Lougheed <david.lougheed@gmail.com>
2024-12-06 15:21:45 -0500

    3: index tr subset without homopolymers

--- 3_benchmarking/2_tr_subset.bash
+++ 3_benchmarking/2_tr_subset.bash
@@ -5,3 +5,4 @@ bcftools view \
   -e 'INFO/TRFperiod=1' \
   -Oz -o data/HG002_GRCh38_TandemRepeats_v1.0.1.no_homopolymers.vcf.gz \
   data/HG002_GRCh38_TandemRepeats_v1.0.1.vcf.gz
+tabix 'data/HG002_GRCh38_TandemRepeats_v1.0.1.no_homopolymers.vcf.gz'
