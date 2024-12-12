f526c95b57580e872cd4c0349e8c32c761252a84
David Lougheed <david.lougheed@gmail.com>
2024-12-12 14:34:00 -0500

    2: add call time parsing + printing util script

---
+++ 2_giab_calls/8_call_time.py
@@ -0,0 +1,41 @@
+import re
+from pathlib import Path
+
+ELAPSED_PATTERN = re.compile(r"((?:\d{1,2}:)?\d{2}:\d{2})(?:\.\d{2})?elapsed")
+
+OUT_DIR = Path(__file__).parent / "out" / "calls"
+TECHS = ("hifi", "ont")
+CALLERS_CORES = {
+    "longtr": 1,
+    "straglr": 8,
+    "strdust": 8,
+    "strkit": 8,
+    "strkit-no-snv": 8,
+    "trgt": 8,
+}
+
+
+def parse_time(time_file: str, cores: int) -> float:
+    time_str = ELAPSED_PATTERN.search(time_file)[1]
+    time_parts = time_str.split(":")
+    if len(time_parts) == 2:
+        time_seconds = int(time_parts[0]) * 60 + int(time_parts[1])
+    elif len(time_parts) == 3:
+        time_seconds = int(time_parts[0]) * 3600 + int(time_parts[0]) * 60 + int(time_parts[1])
+    else:
+        raise NotImplementedError("time_parts must be mm:ss or [h]h:mm:ss")
+
+    return time_seconds * cores
+
+
+def main():
+    for tech in TECHS:
+        for caller, cores in CALLERS_CORES.items():
+            for time_file in (OUT_DIR / tech).glob(f"HG00?.{caller}.*.time"):
+                with open(time_file, "r") as fh:
+                    elapsed_seconds = parse_time(fh.read(), cores)
+                print(f"time_file: {elapsed_seconds}s {elapsed_seconds / 3600:.1f}h")
+
+
+if __name__ == "__main__":
+    main()
