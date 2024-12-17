e3779374fccb08e9ad5b0998f2c57bce22943d23
David Lougheed <david.lougheed@gmail.com>
2024-12-17 10:53:29 -0500

    2: add call percent script

---
+++ 2_giab_calls/9_call_percent.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import pysam
+from pathlib import Path
+
+
+OUT_DIR = Path(__file__).parent / "out" / "calls"
+CALLERS = ("longtr", "straglr", "strdust", "strkit", "strkit-no-snv", "trgt")
+TECHS = ("hifi", "ont")
+
+TOTAL_VARIANTS = 914676  # wc -l out/adotto_catalog_strkit.bed
+
+
+def filter_default(v: pysam.VariantRecord) -> bool:
+    gt = v.samples[0].get("GT")
+    return gt is not None and None not in gt
+
+
+def filter_strkit_snv(v: pysam.VariantRecord) -> bool:
+    return filter_default(v) and v.info.get("VT") == "str"
+
+
+def main():
+    for tech in TECHS:
+        for caller in CALLERS:
+            f = OUT_DIR / tech / f"HG002.{caller}.vcf.gz"
+
+            with pysam.VariantFile(str(f)) as vf:
+                n_called = sum(
+                    1 for _ in filter(filter_default if caller != "strkit" else filter_strkit_snv, vf.fetch())
+                )
+
+            print(f"{tech.rjust(4)} {caller.rjust(15)} {str(n_called).ljust(6)} {n_called / TOTAL_VARIANTS * 100:.2f}%")
+
+
+if __name__ == "__main__":
+    main()
