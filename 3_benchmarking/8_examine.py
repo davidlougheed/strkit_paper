3ea12ac25268f5b12f99c71a211fbd9228cbc657
David Lougheed <david.lougheed@gmail.com>
2024-12-19 12:27:47 -0500

    3: examine

--- 3_benchmarking/8_examine.py
+++ 3_benchmarking/8_examine.py
@@ -1,12 +1,32 @@
+import pandas as pd
 import pysam
 
 
-with open("./data/HG002_GRCh38_TandemRepeats_v1.0.bed", "r") as bf, open("./out/300_400_subset.bed", "w") as of:
-    for line in bf:
-        data = line.strip().split("\t")
-        val = max(abs(int(data[-2])), abs(int(data[-1])))
-        if 300 <= val < 400:
-            of.write(line)
+# req: gunzip -c data/HG002_GRCh38_TandemRepeats_v1.0.bed.gz > data/HG002_GRCh38_TandemRepeats_v1.0.bed
+
+
+# begin: (c) Adam English - see https://github.com/ACEnglish/laytr and https://github.com/ACEnglish/laytr/LICENSE
+
+regions = pd.read_csv("./out/hg002_benchmark/hifi/strkit/refine.regions.txt", sep="\t")
+bed = pd.read_csv(
+    "./data/HG002_GRCh38_TandemRepeats_v1.0.bed",
+    sep="\t",
+    names=["chrom", "start", "end", "tier", "replicates", "var_flag", "entropy", "ad1", "ad2"]
+)
+
+data = regions.join(bed)
+
+# end
+
+print(data)
+
+
+# with open("./data/HG002_GRCh38_TandemRepeats_v1.0.bed", "r") as bf, open("./out/300_400_subset.bed", "w") as of:
+#     for line in bf:
+#         data = line.strip().split("\t")
+#         val = max(abs(int(data[-2])), abs(int(data[-1])))
+#         if 300 <= val < 400:
+#             of.write(line)
 
 
 # vf = pysam.VariantFile("./out/hg002_benchmark/hifi/strkit/fn.vcf.gz")
