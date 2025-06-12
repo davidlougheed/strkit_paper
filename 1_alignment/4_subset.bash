dfd1550560d1077edcf37e3b6250e86270cbd94c
David Lougheed <david.lougheed@gmail.com>
2025-06-12 15:33:23 -0400

    1: add hg002 subsample proportion for ont-simplex

--- 1_alignment/4_subset.bash
+++ 1_alignment/4_subset.bash
@@ -2,8 +2,10 @@
 
 for bf in ./data/ont-simplex/HG00?.aligned.bam; do
   echo "Submitting subset job for ${bf}"
-  frac="TODO"  #TODO: FRAC
-  if "${bf}" == "./data/ont-simplex/HG004.aligned.bam"; then
+  frac="0.6676492"  #TODO: FRAC
+  if "${bf}" == "./data/ont-simplex/HG003.aligned.bam"; then
+    frac="TODO"  #TODO: FRAC
+  elif "${bf}" == "./data/ont-simplex/HG004.aligned.bam"; then
     frac="TODO"  #TODO: FRAC
   fi
   sbatch --export="BAM=${bf},FRAC=${frac}" ./subset_job.bash
