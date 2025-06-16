23177fd182f88a955cabb2cd1364b468d672c66f
David Lougheed <david.lougheed@gmail.com>
2025-06-16 14:13:50 -0400

    1: fix 4_subset if syntax

--- 1_alignment/4_subset.bash
+++ 1_alignment/4_subset.bash
@@ -7,10 +7,10 @@ for bf in ./data/ont-simplex/HG00?.aligned.bam; do
   echo "Submitting subset job for ${bf}"
   # aligned ont-simplex HG002 is at ~48x, we need ~32x for comparability
   frac="0.6676492"
-  if "${bf}" == "./data/ont-simplex/HG003.aligned.bam"; then
+  if [ "${bf}" == "./data/ont-simplex/HG003.aligned.bam" ]; then
     # aligned ont-simplex HG003 @ ~38.7x --> ~31.6x
     frac="0.8181350"
-  elif "${bf}" == "./data/ont-simplex/HG004.aligned.bam"; then
+  elif [ "${bf}" == "./data/ont-simplex/HG004.aligned.bam" ]; then
     # HG004 ont-simplex is already at 30.2452x
     frac="1"  # this will skip subsetting in the job and just link the file instead
   fi
