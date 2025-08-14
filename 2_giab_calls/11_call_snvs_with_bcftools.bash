8133983af009f21b4d693878cda613ac2432fb74
David Lougheed <david.lougheed@gmail.com>
2025-08-14 10:46:34 -0400

    2.11: parallel calling + more time

--- 2_giab_calls/11_call_snvs_with_bcftools.bash
+++ 2_giab_calls/11_call_snvs_with_bcftools.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --mem=16G
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --time=4:00:00
+#SBATCH --cpus-per-task=3
+#SBATCH --time=24:00:00
 #SBATCH --account=rrg-bourqueg-ad
 
 module load bcftools
@@ -12,11 +12,13 @@ HIFI_BAM='../1_alignment/data/hifi/HG002.phased.aligned.bam'
 ONT_SIMPLEX_BAM='../1_alignment/data/ont-simplex/HG002.aligned.subsam.bam'
 ONT_DUPLEX_BAM='../1_alignment/data/ont/HG002.aligned.bam'
 
-bcftools mpileup -Ou -f "${REF}" --threads 4 "${HIFI_BAM}" | \
-  bcftools call --threads 4 -mv -Ob -o out/HG002_snv_calls.hifi.bcf
+bcftools mpileup -Ou -f "${REF}" --threads 2 "${HIFI_BAM}" | \
+  bcftools call --threads 2 --skip-variants indels -mv -Ob -o out/HG002_snv_calls.hifi.bcf &
 
-bcftools mpileup -Ou -f "${REF}" --threads 4 "${ONT_SIMPLEX_BAM}" | \
-  bcftools call --threads 4 -mv -Ob -o out/HG002_snv_calls.ont-simplex.bcf
+bcftools mpileup -Ou -f "${REF}" --threads 2 "${ONT_SIMPLEX_BAM}" | \
+  bcftools call --threads 2 --skip-variants indels -mv -Ob -o out/HG002_snv_calls.ont-simplex.bcf &
 
-bcftools mpileup -Ou -f "${REF}" --threads 4 "${ONT_DUPLEX_BAM}" | \
-  bcftools call --threads 4 -mv -Ob -o out/HG002_snv_calls.ont.bcf
+bcftools mpileup -Ou -f "${REF}" --threads 2 "${ONT_DUPLEX_BAM}" | \
+  bcftools call --threads 2 --skip-variants indels -mv -Ob -o out/HG002_snv_calls.ont.bcf &
+
+wait
