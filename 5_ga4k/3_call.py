9b2bfcf72aeea817d68f17ac38502f49832888bd
David Lougheed <david.lougheed@gmail.com>
2025-09-11 14:54:54 -0400

    ga4k - try to write a resubmission checker...

--- 5_ga4k/3_call.py
+++ 5_ga4k/3_call.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 
 import json
+import os.path
 import subprocess
 
 
 REF = "./data/ref/GRCh38_no_alt_analysis_set.fna"
 SAMPLE_PREFIX = "cmh"
+
+
+def non_zero_time_file(path: str):
+    with open(path, "r") as fh:
+        contents = fh.read()
+    return "non-zero status" in contents
 
 
 def main():
@@ -15,22 +22,26 @@ def main():
     for script in ("longtr", "strdust", "strkit", "strkit-no-snv", "straglr", "trgt"):
         for trio_id, bams in trio_data.items():
             for ind_key, bam in bams.items():
-                subprocess.check_call(" ".join((
-                    "sbatch",
-                    (
-                        f"--export="
-                        f"SEED=1234,"
-                        f"REF={REF},"
-                        f"TECH=hifi,"
-                        f"SAMPLE={SAMPLE_PREFIX}{trio_id}-{ind_key},"
-                        f"HAPLOID_CHRS=X,X,"  # dummy karyotype values - we'll just analyze autosomes
-                        f"KARYOTYPE=XX,"  # "
-                        f"SEX=f,"  # "
-                        f"PHASED=,"
-                        f"BAM={bam}"
-                    ),
-                    f"../2_giab_calls/{script}.bash",
-                )), shell=True)
+                sample = f"{SAMPLE_PREFIX}{trio_id}-{ind_key}"
+                time_file = f"./out/calls/hifi/{sample}.{script}.time"
+                if not os.path.exists(time_file) or non_zero_time_file(time_file):
+                    # hacky resubmission check - if time file not found or exists but mentions a non-zero status code.
+                    subprocess.check_call(" ".join((
+                        "sbatch",
+                        (
+                            f"--export="
+                            f"SEED=1234,"
+                            f"REF={REF},"
+                            f"TECH=hifi,"
+                            f"SAMPLE={sample},"
+                            f"HAPLOID_CHRS=X,X,"  # dummy karyotype values - we'll just analyze autosomes
+                            f"KARYOTYPE=XX,"  # "
+                            f"SEX=f,"  # "
+                            f"PHASED=,"
+                            f"BAM={bam}"
+                        ),
+                        f"../2_giab_calls/{script}.bash",
+                    )), shell=True)
 
 
 if __name__ == "__main__":
