ecd2431a14fe3b4e4bca0d5a0323e77d7d976b4b
David Lougheed <david.lougheed@gmail.com>
2025-09-16 16:53:35 -0400

    5: add ga4k coverages calculator script

---
+++ 5_ga4k/7_ga4k_coverages.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# Needs `module load samtools` first
+
+import orjson
+import polars
+import polars as pl
+import subprocess
+
+
+def get_bam_depth(bf: str) -> int:
+    p_depth = subprocess.Popen(("samtools", "depth", "-a", bf), stdout=subprocess.PIPE)
+    res = subprocess.check_output(("awk", '{sum+=$3} END { print "Average = ",sum/NR}'), stdin=p_depth.stdout)
+    p_depth.wait()
+    return int(res.decode("ascii"))
+
+
+def main():
+    records = []
+
+    with open("./data/trios.json", "rb") as fh:
+        trios = orjson.loads(fh.read())
+
+    for idx, (trio_id, trio_data) in enumerate(trios.items(), 1):
+        child_bam = trio_data["1"]
+        p1_bam = trio_data["2"]
+        p2_bam = trio_data["3"]
+        child_depth = get_bam_depth(child_bam)
+        p1_depth = get_bam_depth(p1_bam)
+        p2_depth = get_bam_depth(p2_bam)
+
+        records.append({"idx": idx, "trio": trio_id, "child": child_depth, "p1": p1_depth, "p2": p2_depth})
+
+    df = polars.from_dicts(records)
+    with pl.Config(tbl_rows=50):
+        print(df)
+
+
+if __name__ == "__main__":
+    main()
